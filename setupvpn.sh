277335136a0ea0d55309e1e6a0411b3df0b4e601
TsimermanE <tsimer123@mail.ru>
2021-08-15 15:02:04 +0300

    Second commit - setap VPN - fix M_DB 4.4 & key repo

--- setupvpn.sh
+++ setupvpn.sh
@@ -1,9 +1,9 @@
 cat <<EOF> setupvpn.sh
 #!/bin/bash
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
-echo "deb http://repo.pritunl.com/stable/apt xenial main" > /etc/apt/sources.list.d/pritunl.list
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 718D4B7C549A058F8B6B
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+echo "deb https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+echo "deb http://repo.pritunl.com/stable/apt focal main" > /etc/apt/sources.list.d/pritunl.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 656408E390CFB1F5
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7AE645C0CF8E292A
 apt-get --assume-yes update
 apt-get --assume-yes upgrade
 apt-get --assume-yes install pritunl mongodb-org
